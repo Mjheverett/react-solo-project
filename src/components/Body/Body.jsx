eca6467a7ac13ef82940043d5f28b8495ce74589
Matthew Everett <matthew@mjheverett.com>
2020-10-23 16:28:12 -0400

    Initial Commit

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = ()=> {
+    return (
+        <p>content</p>
+    )
+}
+
+export default Body;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-nav">
+                <h1>New App Name</h1>
+            </div>
+            <div className="header-nav">
+                
+            </div>
+            <div className="header-nav">
+                <h4>Login</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Header; 
