18330efc74e7c5b349721a9c29f5977012216b26
Matthew Everett <matthew@mjheverett.com>
2020-10-26 10:42:25 -0400

    React-dates and basic form created

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -1,27 +1,95 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React, { Component } from 'react';
+
 import moment from 'moment';
 
-const Booking = () => {
-    const [date, setDate] = useState(new Date());
+import 'react-dates/initialize';
+import { DateRangePicker, DayPickerRangeController } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import { render } from '@testing-library/react';
 
-    const _handleDateChange = (newDate) => {
-        console.log("newDate", newDate);
-        setDate(newDate);
+import 'bulma/css/bulma.css';
+import { Field, Label, Control, Input, Title, Select, TextArea, Checkbox, Button } from 'bloomer';
+
+class Booking extends Component {
+    state = {
+        startDate: '',
+        endDate: '',
+        focusedInput: '',
     }
     
-    return (
-        <>
-            <h4>Book your stay!</h4>
-            <Calendar 
-                onChange={_handleDateChange}
-                selectRange={true}
-                value={date}
-            />
-            <p>Selected dates: {moment(date[0]).format('MMMM Do YYYY')} - {moment(date[1]).format('MMMM Do YYYY')}</p>
-        </>
-    )
+    render() {
+
+        return (
+            <>
+                <br />
+                <Title isSize={4}>Book your stay!</Title>
+    
+                <DateRangePicker
+                    startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                    startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                    endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                    endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                    onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                    focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                    onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                />
+                
+                <form>
+                    <br />
+                    <Field>
+                        <Label>First Name</Label>
+                        <Control>
+                            <Input type="text" placeholder='Text Input' />
+                        </Control>
+                    </Field>
+
+                    <Field>
+                        <Label>Last Name</Label>
+                        <Control>
+                            <Input type="text" placeholder='Text Input' />
+                        </Control>
+                    </Field>
+
+                    <Field>
+                        <Label>Number of Guests:</Label>
+                        <Control>
+                            <Select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                            </Select>
+                        </Control>
+                    </Field>
+
+                    <Field>
+                        <Label>Message/Special Instructions</Label>
+                        <Control>
+                            <TextArea placeholder={'Message to the Owner...'} />
+                        </Control>
+                    </Field>
+
+                    <Field>
+                        <Label>Terms and Conditions</Label>
+                        <Control>
+                            <Checkbox> I agree </Checkbox>
+                        </Control>
+                    </Field>
+
+                    <Field isGrouped='centered'>
+                        <Control>
+                            <Button isLink>Submit</Button>
+                        </Control>
+                        <Control>
+                            <Button isColor='danger'>Cancel</Button>
+                        </Control>
+                    </Field>
+                </form>
+            </>
+        )
+    }
 }
 
 export default Booking;
