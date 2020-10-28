68374fea2ea441cfd59786a9cd4effee04b5d36e
Matthew Everett <matthew@mjheverett.com>
2020-10-27 21:21:56 -0400

    Working on date formatting

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -77,7 +77,7 @@ class Booking extends Component {
                         startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
                         endDate={this.state.endDate} // momentPropTypes.momentObj or null,
                         endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-                        onDatesChange={({ startDate, endDate }) => this._dateTest({ startDate, endDate })} // PropTypes.func.isRequired,
+                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
                         focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                         onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
                         required={true}
